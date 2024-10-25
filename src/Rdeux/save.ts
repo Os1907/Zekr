09ea0e2359aec175efb3056790fa635c61378d4e
os1907 <osamatrezey@gmail.com>
2024-10-25 22:30:53 +0300

    fixSaved

--- src/Rdeux/save.ts
+++ src/Rdeux/save.ts
@@ -17,18 +17,14 @@ const save = createSlice({
   reducers: {
     store(state, action: PayloadAction<any>) {
         state.push(action.payload);
-        // localStorage.setItem("data", JSON.stringify(state));
         if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
             localStorage.setItem("data", JSON.stringify(state));
-            // console.log(" data ==>", action.payload);
           }
     },
     remove(state, action: PayloadAction<any>) {
       state.splice(action.payload, 1);
         if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
             localStorage.setItem("data", JSON.stringify(state.splice(action.payload, 1)));
-          //  console.log( state.findIndex((item) => item === action.payload));
-           
           }
     },
     empty(state) {
